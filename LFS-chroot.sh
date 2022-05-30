bda925a78706b428301b5eb42cf43c6ca4088768
Voxol <voxol.tetra@protonmail.com>
2022-05-30 15:33:32 -0400

    Added chroot script

---
+++ LFS-chroot.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+LFS=/mnt/scratch
+
+echo "\$LFS is set to $LFS..."
+echo "If this is not correct, abort NOW."
+echo
+echo "Continuing in 5 seconds..."
+sleep 5
+
+if [ "$(id -u)" = "0" ]; then
+    echo "Mounting /dev..."
+    mount -v --bind /dev $LFS/dev
+    echo
+    echo "Mounting virtual kernel file systems..."
+    mount -v --bind /dev/pts $LFS/dev/pts
+    mount -vt proc proc $LFS/proc
+    mount -vt sysfs sysfs $LFS/sys
+    mount -vt tmpfs tmpfs $LFS/run
+    echo
+    echo "Necessary filesystem mount complete. Chrooting into LFS..."
+    sleep 2
+
+    chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login
+
+else
+    echo "This script must be run as root to chroot into LFS!"
+    exit 1
+fi
